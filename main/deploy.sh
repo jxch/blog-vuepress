921d1c2a5dcc191594d07741732bec71db3ea050
jxch <qianbuhan@proton.me>
2026-01-15 10:19:34 +0800

    deploy

--- main/deploy.sh
+++ main/deploy.sh
@@ -5,4 +5,4 @@ git add -A
 git commit -m 'deploy'
 
 git checkout -B vuepress-page
-git push -f git@github.com-jxch:jxch/jxch.github.io.git vuepress-page
+git push -f git@github.com:jxch/jxch.github.io.git vuepress-page
